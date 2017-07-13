fd0c9e3525a8b54c585639dae28c541ef39576ed
kexiaojiu <kexiaojiu@163.com>
2017-07-13 19:57:54 +0800

    group the countries by their populations

--- download_data/population_distribution_map/world_population_group.py
+++ download_data/population_distribution_map/world_population_group.py
@@ -2,6 +2,8 @@
 import json
 from country_codes import get_country_code
 import pygal
+from pygal.style import RotateStyle as RS
+from pygal.style import LightColorizedStyle as LCS
 
 # 将数据加载到一个列表中
 filename = 'population_data.json'
@@ -27,11 +29,24 @@ for pop_dict in pop_data:
                 cc_populations[code] = population
             else:
                 print("ERROR - " + country_name + ": " + str(population))
-            
+
+# 根据人口把国家分为三组            
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+    
 
 # 绘制世界人口分布地图
-wm = pygal.maps.world.World()
+wm_style = RS('#336699', base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
 wm.title = "World Population in 2010, by Country" 
-wm.add('2010', cc_populations)
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('> 1bn', cc_pops_3)
 
-wm.render_to_file('world_population.svg')
+wm.render_to_file('world_population_group.svg')
